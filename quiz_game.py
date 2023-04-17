678ef17c6ba6cb513d6715ff1e7877d68695c402
mindmotivate <130941970+mindmotivate@users.noreply.github.com>
2023-04-16 20:31:05 -0400

    Add files via upload

---
+++ quiz_game.py
@@ -0,0 +1,39 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+
+==== RESTART: C:\Users\Owner\Desktop\My Python IDE Folder\number_guesser.py ====
+>>> Type A Number: 10
+Make a guess: 5
+You were above the number!
+Make a guess: 3
+You were above the number!
+Make a guess: 2
+You were above the number!
+Make a guess: 1
+You got it!
+You got it in 4 guesses
+>>> 
+==== RESTART: C:\Users\Owner\Desktop\My Python IDE Folder\number_guesser.py ====
+Type A Number: 50
+Make a guess: 2
+You were below the number!
+Make a guess: 25
+You were below the number!
+Make a guess: 65
+You were above the number!
+Make a guess: 60
+You were above the number!
+Make a guess: 58
+You were above the number!
+Make a guess: 50
+You were above the number!
+Make a guess: 47
+You were above the number!
+Make a guess: 42
+You were below the number!'
+Make a guess: 47
+You were above the number!
+Make a guess: 44
+You got it!
+You got it in 10 guesses
