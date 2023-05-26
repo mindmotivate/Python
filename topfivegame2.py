1d965941cb672c581217132d00b15e1a7cb61ca2
mindmotivate <130941970+mindmotivate@users.noreply.github.com>
2023-05-26 17:55:51 -0400

    Add files via upload
    Enter your top Top 5 basketball athletes in order from Top to Least

---
+++ topfivegame2.py
@@ -0,0 +1,36 @@
+
+print("Welcome to the Top Five Ranking System!")
+print("Please Enter Your First Name: ")
+
+name = input()
+
+print( name + " Please Rank Your Top 5 NBA Players from Worst to Greatest, With The Greatest Being Ranked Number 1")
+
+
+print("Please Enter Your First Choice: ")
+number1 = input()
+
+print("Please Enter Your Second Choice: ")
+number2 = input()
+
+print("Please Enter Your Third Choice: ")
+number3 = input()
+
+print("Please Enter Your Fourth Choice: ")
+number4 = input()
+
+print("Please Enter Your Last Choice: ")
+number5 = input()
+
+
+print("Your selections have been completed!")
+
+print("In your opinion, the TOP 5 ranking are as follows: ")
+
+print( "1st: " + number1)
+print( "2nd: " + number2)
+print( "3rd: " + number3)
+print( "4th: " + number4)
+print( "5th: " + number5)
+
+print(" Thanks for playing! ")
